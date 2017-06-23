8c8b33c900c31da0452df3242ac87d5d4a4b61bc
ADMINISTRATOR\Administrator <duc.bui8@gmail.com>
2017-06-23 21:36:10 +0700

    Test app only

--- app.py
+++ app.py
@@ -26,67 +26,67 @@ class Item(Document):
     price = FloatField()
 
 
-@app.route("/add_bitch", methods=["GET","POST"])
-def add_bitch():
-    if request.method == "GET":
-        return render_template("add_item.html")
-    elif request.method == "POST": #submit form
-        ##Step 1: get data from form
-        form = request.form
-        title = form['title']
-        price = form['price']
-
-        image = request.files['image']
-        filename = secure_filename(image.filename)
-        save_location = os.path.join(app.config["IMG_PATH"], filename)
-        image.save(save_location)
-        ##Step 2: Create data
-        if title == '' or image == '' or price == '':
-            return render_template("666.html")
-        else:
-            item = Item(title=title, image=url_for("image",image_name= filename),price=price)
-            item.save()
-        ##Step 3: Redirect
-        return  redirect(url_for("index"))
+# @app.route("/add_bitch", methods=["GET","POST"])
+# def add_bitch():
+#     if request.method == "GET":
+#         return render_template("add_item.html")
+#     elif request.method == "POST": #submit form
+#         ##Step 1: get data from form
+#         form = request.form
+#         title = form['title']
+#         price = form['price']
+#
+#         image = request.files['image']
+#         filename = secure_filename(image.filename)
+#         save_location = os.path.join(app.config["IMG_PATH"], filename)
+#         image.save(save_location)
+#         ##Step 2: Create data
+#         if title == '' or image == '' or price == '':
+#             return render_template("666.html")
+#         else:
+#             item = Item(title=title, image=url_for("image",image_name= filename),price=price)
+#             item.save()
+#         ##Step 3: Redirect
+#         return  redirect(url_for("index"))
 
 @app.route('/')
 def index():
     return render_template("index.html",items = Item.objects())
 
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
-
-@app.route("/images/<image_name>")
-def image(image_name):
-    return send_from_directory(app.config["IMG_PATH"], image_name)
-###
-# The functions below should be applicable to all Flask apps.
-###
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
-
+# @app.route('/about/')
+# def about():
+#     """Render the website's about page."""
+#     return render_template('about.html')
+#
+# @app.route("/images/<image_name>")
+# def image(image_name):
+#     return send_from_directory(app.config["IMG_PATH"], image_name)
+# ###
+# # The functions below should be applicable to all Flask apps.
+# ###
+# @app.route('/<file_name>.txt')
+# def send_text_file(file_name):
+#     """Send your static text file."""
+#     file_dot_text = file_name + '.txt'
+#     return app.send_static_file(file_dot_text)
+#
+#
+# @app.after_request
+# def add_header(response):
+#     """
+#     Add headers to both force latest IE rendering engine or Chrome Frame,
+#     and also to cache the rendered page for 10 minutes.
+#     """
+#     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+#     response.headers['Cache-Control'] = 'public, max-age=600'
+#     return response
+#
+#
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     """Custom 404 page."""
+#     return render_template('404.html'), 404
+#
 
 if __name__ == '__main__':
     app.run(debug=False)
